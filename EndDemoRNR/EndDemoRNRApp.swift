8cb32a64d91236ff682b439a29db4cebd38eccdb
Robert Redmond <robert_redmond@yahoo.com>
2025-06-19 16:09:42 +0100

    Initial Commit

---
+++ EndDemoRNR/EndDemoRNRApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndDemoRNRApp.swift
+//  EndDemoRNR
+//
+//  Created by Robert Redmond on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct EndDemoRNRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
